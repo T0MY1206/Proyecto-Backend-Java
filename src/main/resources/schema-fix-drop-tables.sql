46c6bc40c9cd40dd83291ffaa665b793d5fc1de1
Tomas <tomas2000tutor@hotmail.com>
2026-03-11 21:55:03 -0300

    Delete schema-fix-drop-tables.sql

--- src/main/resources/schema-fix-drop-tables.sql
+++
@@ -1,9 +0,0 @@
--- Ejecutar en la base de datos (Supabase SQL Editor o psql) si aparecen errores
--- "there is no primary key for referenced table". Las tablas existían sin PK;
--- al borrarlas, al reiniciar la app Hibernate las creará bien.
--- Orden: primero las que tienen FK, luego las referenciadas.
-
-DROP TABLE IF EXISTS detalle_venta;
-DROP TABLE IF EXISTS venta;
-DROP TABLE IF EXISTS producto;
-DROP TABLE IF EXISTS sucursal;
